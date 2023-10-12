4b6753e3a1b832cfd057cba6c3a01d7815882376
Adnan525 <adnaan525@gmail.com>
2023-10-12 21:16:24 +1100

    shows the name of the circuit when selected from drop down menu

--- f1_shiny.R
+++ f1_shiny.R
@@ -7,37 +7,44 @@ circuits <- read.csv("data/circuits.csv")
 names(circuits)[3] <- "circuit_name"
 
 # variables
-circuit_options_vector  = unique(circuits$circuit_name)
+circuit_options_vector <- unique(circuits$circuit_name)
 
-
-ui <- fluidPage(
-  "F1 Analysis"
-)
-server <- function(input, output, session){
-  
-}
-library(shiny)
-
+# shiny
 ui <- fluidPage(
   titlePanel("F1 Analysis"),
   sidebarLayout(
     sidebarPanel(
-      selectInput("Topics", label = "Topics", choices = c("Please select a topic", "Circuits", "Drives")),
+      selectInput("topics", label = "Topics", choices = c("Please select a topic", "Circuits", "Drives")),
       conditionalPanel(
-        condition = "input.Topics == 'Circuits'",
-        selectizeInput("search for circuits", label = "Search", choices = circuit_options_vector, multiple = FALSE)
+        condition = "input.topics == 'Circuits'",
+        selectizeInput("circuit_search", label = "Select a circuit", choices = circuit_options_vector, multiple = FALSE)
       )
     ),
     mainPanel(
       img(src = "dataset-cover.jpg", height = 140, width = 800),
+      textOutput("selected_circuit_text")
     )
   )
 )
 
-
 server <- function(input, output) {
+  # selected circuit
+  selected_circuit <- reactiveVal(NULL)
+  observeEvent(input$circuit_search, {
+    selected_circuit(input$circuit_search)
+  })
+  
+  output$selected_circuit_text <- renderText({
+    selected <- selected_circuit()
+    if(input$topics == "Circuits")
+    {
+      if (!is.null(selected)) {
+        paste("You selected:", selected)
+      } else {
+        "Please select a circuit."
+      }
+    }
+  })
 }
-
-
 
 shinyApp(ui, server)
