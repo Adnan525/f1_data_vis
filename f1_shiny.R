d2920df5fb0f142a30cba74c77ab860009fb524e
Adnan525 <adnaan525@gmail.com>
2023-10-12 20:44:18 +1100

    cound not do mapview, will try later

--- f1_shiny.R
+++ f1_shiny.R
@@ -1,4 +1,15 @@
 library(shiny)
+library(sf)
+library(mapview)
+
+# dataset
+circuits <- read.csv("data/circuits.csv")
+names(circuits)[3] <- "circuit_name"
+
+# variables
+circuit_options_vector  = unique(circuits$circuit_name)
+
+
 ui <- fluidPage(
   "F1 Analysis"
 )
@@ -8,11 +19,25 @@ server <- function(input, output, session){
 library(shiny)
 
 ui <- fluidPage(
-  "F1 Analysis"
+  titlePanel("F1 Analysis"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("Topics", label = "Topics", choices = c("Please select a topic", "Circuits", "Drives")),
+      conditionalPanel(
+        condition = "input.Topics == 'Circuits'",
+        selectizeInput("search for circuits", label = "Search", choices = circuit_options_vector, multiple = FALSE)
+      )
+    ),
+    mainPanel(
+      img(src = "dataset-cover.jpg", height = 140, width = 800),
+    )
+  )
 )
 
-server <- function(input, output, session) {
-  
+
+server <- function(input, output) {
 }
+
+
 
 shinyApp(ui, server)
