f37b5050b5905575b78f0e206d4ac4f79c1d6e90
Adnan525 <adnaan525@gmail.com>
2023-10-12 18:57:33 +1100

    first shiny code added

---
+++ f1_shiny.R
@@ -0,0 +1,18 @@
+library(shiny)
+ui <- fluidPage(
+  "F1 Analysis"
+)
+server <- function(input, output, session){
+  
+}
+library(shiny)
+
+ui <- fluidPage(
+  "F1 Analysis"
+)
+
+server <- function(input, output, session) {
+  
+}
+
+shinyApp(ui, server)
