c4bdd376bf87f639d86d259c60e6f9a58cedad37
Adnan525 <adnaan525@gmail.com>
2023-10-17 04:19:38 +1100

    driver details- got name, isCurrent, currentTeam, allTeam and totalWin

---
+++ README.md
@@ -0,0 +1,6 @@
+# Analysis on why Mich Schumacher was axed from Haas F1 on 2023 and replace by Nico Hulkenberg
+
+-   3 rooikes in the seson - Mick, Yuki, Nikita
+-   driver standings yuki and mick
+-   team mate comparison haas and alphatauri
+-   alphatauri other driver's details
---
+++ driver_details.R
@@ -0,0 +1,51 @@
+drivers <- read.csv("data/drivers.csv")
+drivers <- drivers %>% mutate(driver_name = paste(forename, surname))
+driver_standings <- read.csv("data/driver_standings.csv")
+races <- read.csv("data/races.csv")
+races <- races %>% rename(race_name = name)
+results <- read.csv("data/results.csv")
+constructors <- read.csv("data/constructors.csv")
+constructors <- constructors %>% rename(constructor_name = name)
+
+# driver name
+# constructor
+# still driving?
+# total wins
+# total podiums
+# total points
+
+target <- driver_standings %>% 
+  # select(-driverStandingsId) %>% 
+  inner_join(drivers, by = "driverId") %>% 
+  select(driver_name, names(driver_standings))
+
+target <- target %>% 
+  inner_join(results, by = c("raceId", "driverId")) %>% 
+  rename(points = points.x, position = position.x, positionText = positionText.x)%>% 
+  select(constructorId, names(target))
+
+target <- target %>% 
+  inner_join(constructors, by = "constructorId") %>% 
+  select(constructor_name, names(target))
+
+target <- target %>% 
+  inner_join(races, by = "raceId") %>% 
+  select(year, race_name, names(target))
+
+
+# test
+# we want to know about Valtteri Bottas
+target_driver <- "Valtteri Bottas"
+current_year <- 2023
+
+target_driver_df <- target %>% filter(driver_name == target_driver) %>% arrange(year)
+is_currently_driving <- ifelse(current_year %in% unique(target_driver_df$year), TRUE, FALSE)
+current_team <- ifelse(is_currently_driving,
+                       unique(target_driver_df$constructor_name)[length(unique(target_driver_df$constructor_name))],
+                       NA)
+all_teams <- unique(target_driver_df$constructor_name)
+total_win <- target_driver_df %>% 
+  group_by(year) %>% 
+  summarise(win_by_year = max(wins)) %>% 
+  summarise(total_win = sum(win_by_year)) %>% 
+  pull(total_win)
