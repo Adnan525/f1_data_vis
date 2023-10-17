d884d7d136adfb6f462087720ed94e00e85fb036
Adnan525 <adnaan525@gmail.com>
2023-10-17 23:33:56 +1100

    driver details - num_championship added

--- driver_details.R
+++ driver_details.R
@@ -39,16 +39,23 @@ target_driver <- "Valtteri Bottas"
 current_year <- 2023
 
 target_driver_df <- target %>% filter(driver_name == target_driver) %>% arrange(year)
+
+target_driver_appearance <- dim(target_driver_df)[1]
+
 is_currently_driving <- ifelse(current_year %in% unique(target_driver_df$year), TRUE, FALSE)
+
 current_team <- ifelse(is_currently_driving,
                        unique(target_driver_df$constructor_name)[length(unique(target_driver_df$constructor_name))],
                        NA)
+
 all_teams <- unique(target_driver_df$constructor_name)
+
 total_win <- target_driver_df %>% 
   group_by(year) %>% 
   summarise(win_by_year = max(wins)) %>% 
   summarise(total_win = sum(win_by_year)) %>% 
   pull(total_win)
+
 team_wise_win <- target_driver_df %>% 
   group_by(constructor_name, year) %>% 
   summarise(total_win = max(wins)) %>% 
@@ -61,17 +68,28 @@ total_podium <- target_driver_df %>%
   # filter(podium_bin == 1)
   summarise(total_podium = sum(podium_bin)) %>%
   pull(total_podium)
+
 team_wise_podium <- target_driver_df %>% 
   mutate(podium_bin = ifelse(position <= 3, 1, 0)) %>% 
   group_by(constructor_name) %>% 
   summarise(podium_by_team = sum(podium_bin))
+
 total_points <- target_driver_df %>% 
   group_by(year) %>% 
   summarise(points_by_year = max(points)) %>% 
   summarise(total_points = sum(points_by_year)) %>% 
   pull(total_points)
+
 team_wise_point <- target_driver_df %>% 
   group_by(constructor_name, year) %>% 
   summarise(point_by_year = max(points)) %>% 
   group_by(constructor_name) %>% 
   summarise(point_by_team = sum(point_by_year))
+
+# championship_count
+championship_df <- target %>% group_by(year) %>% 
+  filter(points == max(points)) %>%
+  arrange(year) %>% 
+  select(year, constructor_name, driver_name, points)
+temp <- table(championship_df$driver_name)
+target_driver_championship <- ifelse(target_driver %in% temp, temp[target_driver], 0)
